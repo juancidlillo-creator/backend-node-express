dcecb2d06430c20ea5fa5c07ced06e7127f2040f
juancidlillo-creator <juan.cidlillo@gmail.com>
2026-09-05 15:30:01 -0400

    Configuracion de dependencias, scripts de npm y comentarios en app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-import { log } from 'console';
+// Carga las variables de entorno desde el archivo .env a process.env
 import 'dotenv/config';
+// Importa el framework Express para la creación del servidor y gestión de rutas
+import express from 'express';
 
+// Inicializa la aplicación de Express
+const app = express();
 
+// Define el puerto del servidor desde process.env.PORT; si no existe, asigna 3000 por defecto
 const PORT = process.env.PORT || 3000;
 
-function ejecutarServidor(){
-    console.log(`Servidor iniciado en el puerto ${PORT}`);
-}
+// Configura una ruta de prueba básica en la raíz
+app.get('/', (req, res) => {
+    res.send('Servidor en ejecución');
+});
 
-ejecutarServidor()
+// Pone el servidor a escuchar en el puerto 3000 
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado y escuchando en el puerto ${PORT}`);
+});
